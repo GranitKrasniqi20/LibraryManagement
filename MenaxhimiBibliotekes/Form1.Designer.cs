24322090850eb78c302cfeae58976f43ea2f07e8
Beis Presheva <beis.presheva@riinvest.net>
2020-04-19 15:13:21 +0200

    UI: Created Menu and added img resources

--- MenaxhimiBibliotekes/Form1.Designer.cs
+++ MenaxhimiBibliotekes/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace MenaxhimiBibliotekes
 {
-    partial class Form1
+    partial class mainForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,21 +28,219 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainForm));
+            this.panelMenu = new System.Windows.Forms.Panel();
+            this.appLogo = new System.Windows.Forms.PictureBox();
+            this.appName = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnDashboard = new System.Windows.Forms.Button();
+            this.btnMembers = new System.Windows.Forms.Button();
+            this.btnMaterials = new System.Windows.Forms.Button();
+            this.btnNotifications = new System.Windows.Forms.Button();
+            this.btnSettings = new System.Windows.Forms.Button();
+            this.btnLoggedUser = new System.Windows.Forms.Button();
+            this.btnLogout = new System.Windows.Forms.Button();
+            this.panelMenu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.appLogo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.appName)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // Form1
+            // panelMenu
+            // 
+            this.panelMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(55)))), ((int)(((byte)(132)))));
+            this.panelMenu.Controls.Add(this.btnSettings);
+            this.panelMenu.Controls.Add(this.btnNotifications);
+            this.panelMenu.Controls.Add(this.btnMaterials);
+            this.panelMenu.Controls.Add(this.btnMembers);
+            this.panelMenu.Controls.Add(this.btnDashboard);
+            this.panelMenu.Controls.Add(this.panel1);
+            this.panelMenu.Controls.Add(this.appLogo);
+            this.panelMenu.Controls.Add(this.appName);
+            this.panelMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelMenu.Location = new System.Drawing.Point(0, 0);
+            this.panelMenu.Name = "panelMenu";
+            this.panelMenu.Size = new System.Drawing.Size(273, 811);
+            this.panelMenu.TabIndex = 1;
+            // 
+            // appLogo
+            // 
+            this.appLogo.Image = ((System.Drawing.Image)(resources.GetObject("appLogo.Image")));
+            this.appLogo.Location = new System.Drawing.Point(94, 12);
+            this.appLogo.Name = "appLogo";
+            this.appLogo.Size = new System.Drawing.Size(82, 82);
+            this.appLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.appLogo.TabIndex = 0;
+            this.appLogo.TabStop = false;
+            // 
+            // appName
+            // 
+            this.appName.Image = ((System.Drawing.Image)(resources.GetObject("appName.Image")));
+            this.appName.Location = new System.Drawing.Point(3, 75);
+            this.appName.Name = "appName";
+            this.appName.Size = new System.Drawing.Size(267, 82);
+            this.appName.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.appName.TabIndex = 0;
+            this.appName.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(48)))), ((int)(((byte)(115)))));
+            this.panel1.Controls.Add(this.btnLogout);
+            this.panel1.Controls.Add(this.btnLoggedUser);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 653);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(273, 158);
+            this.panel1.TabIndex = 1;
+            // 
+            // btnDashboard
+            // 
+            this.btnDashboard.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(55)))), ((int)(((byte)(132)))));
+            this.btnDashboard.FlatAppearance.BorderSize = 0;
+            this.btnDashboard.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDashboard.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDashboard.ForeColor = System.Drawing.Color.White;
+            this.btnDashboard.Image = ((System.Drawing.Image)(resources.GetObject("btnDashboard.Image")));
+            this.btnDashboard.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnDashboard.Location = new System.Drawing.Point(0, 202);
+            this.btnDashboard.Name = "btnDashboard";
+            this.btnDashboard.Padding = new System.Windows.Forms.Padding(15, 0, 0, 0);
+            this.btnDashboard.Size = new System.Drawing.Size(273, 70);
+            this.btnDashboard.TabIndex = 2;
+            this.btnDashboard.Text = "Dashboard";
+            this.btnDashboard.UseVisualStyleBackColor = false;
+            // 
+            // btnMembers
+            // 
+            this.btnMembers.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(55)))), ((int)(((byte)(132)))));
+            this.btnMembers.FlatAppearance.BorderSize = 0;
+            this.btnMembers.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMembers.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMembers.ForeColor = System.Drawing.Color.White;
+            this.btnMembers.Image = ((System.Drawing.Image)(resources.GetObject("btnMembers.Image")));
+            this.btnMembers.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnMembers.Location = new System.Drawing.Point(0, 274);
+            this.btnMembers.Name = "btnMembers";
+            this.btnMembers.Padding = new System.Windows.Forms.Padding(15, 0, 0, 0);
+            this.btnMembers.Size = new System.Drawing.Size(273, 70);
+            this.btnMembers.TabIndex = 2;
+            this.btnMembers.Text = "Members";
+            this.btnMembers.UseVisualStyleBackColor = false;
+            // 
+            // btnMaterials
+            // 
+            this.btnMaterials.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(55)))), ((int)(((byte)(132)))));
+            this.btnMaterials.FlatAppearance.BorderSize = 0;
+            this.btnMaterials.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMaterials.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMaterials.ForeColor = System.Drawing.Color.White;
+            this.btnMaterials.Image = ((System.Drawing.Image)(resources.GetObject("btnMaterials.Image")));
+            this.btnMaterials.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnMaterials.Location = new System.Drawing.Point(0, 346);
+            this.btnMaterials.Name = "btnMaterials";
+            this.btnMaterials.Padding = new System.Windows.Forms.Padding(15, 0, 0, 0);
+            this.btnMaterials.Size = new System.Drawing.Size(273, 70);
+            this.btnMaterials.TabIndex = 2;
+            this.btnMaterials.Text = "Materials";
+            this.btnMaterials.UseVisualStyleBackColor = false;
+            // 
+            // btnNotifications
+            // 
+            this.btnNotifications.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(55)))), ((int)(((byte)(132)))));
+            this.btnNotifications.FlatAppearance.BorderSize = 0;
+            this.btnNotifications.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNotifications.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNotifications.ForeColor = System.Drawing.Color.White;
+            this.btnNotifications.Image = ((System.Drawing.Image)(resources.GetObject("btnNotifications.Image")));
+            this.btnNotifications.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnNotifications.Location = new System.Drawing.Point(0, 418);
+            this.btnNotifications.Name = "btnNotifications";
+            this.btnNotifications.Padding = new System.Windows.Forms.Padding(15, 0, 0, 0);
+            this.btnNotifications.Size = new System.Drawing.Size(273, 70);
+            this.btnNotifications.TabIndex = 2;
+            this.btnNotifications.Text = "Notifications";
+            this.btnNotifications.UseVisualStyleBackColor = false;
+            // 
+            // btnSettings
+            // 
+            this.btnSettings.FlatAppearance.BorderSize = 0;
+            this.btnSettings.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSettings.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSettings.ForeColor = System.Drawing.Color.White;
+            this.btnSettings.Image = ((System.Drawing.Image)(resources.GetObject("btnSettings.Image")));
+            this.btnSettings.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSettings.Location = new System.Drawing.Point(0, 490);
+            this.btnSettings.Name = "btnSettings";
+            this.btnSettings.Padding = new System.Windows.Forms.Padding(15, 0, 0, 0);
+            this.btnSettings.Size = new System.Drawing.Size(273, 70);
+            this.btnSettings.TabIndex = 2;
+            this.btnSettings.Text = "Settings";
+            this.btnSettings.UseVisualStyleBackColor = true;
+            // 
+            // btnLoggedUser
+            // 
+            this.btnLoggedUser.FlatAppearance.BorderSize = 0;
+            this.btnLoggedUser.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLoggedUser.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLoggedUser.ForeColor = System.Drawing.Color.White;
+            this.btnLoggedUser.Image = ((System.Drawing.Image)(resources.GetObject("btnLoggedUser.Image")));
+            this.btnLoggedUser.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnLoggedUser.Location = new System.Drawing.Point(0, 3);
+            this.btnLoggedUser.Name = "btnLoggedUser";
+            this.btnLoggedUser.Padding = new System.Windows.Forms.Padding(15, 0, 0, 0);
+            this.btnLoggedUser.Size = new System.Drawing.Size(273, 70);
+            this.btnLoggedUser.TabIndex = 2;
+            this.btnLoggedUser.Text = "Beis Presheva";
+            this.btnLoggedUser.UseVisualStyleBackColor = true;
+            // 
+            // btnLogout
+            // 
+            this.btnLogout.FlatAppearance.BorderSize = 0;
+            this.btnLogout.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogout.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogout.ForeColor = System.Drawing.Color.White;
+            this.btnLogout.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnLogout.Location = new System.Drawing.Point(0, 76);
+            this.btnLogout.Name = "btnLogout";
+            this.btnLogout.Padding = new System.Windows.Forms.Padding(15, 0, 0, 0);
+            this.btnLogout.Size = new System.Drawing.Size(273, 70);
+            this.btnLogout.TabIndex = 2;
+            this.btnLogout.Text = "Log Out";
+            this.btnLogout.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnLogout.UseVisualStyleBackColor = true;
+            // 
+            // mainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1184, 811);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1234, 811);
+            this.Controls.Add(this.panelMenu);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MinimumSize = new System.Drawing.Size(1200, 850);
+            this.Name = "mainForm";
+            this.Text = "Stackbooks Management System";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.panelMenu.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.appLogo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.appName)).EndInit();
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+        private System.Windows.Forms.Panel panelMenu;
+        private System.Windows.Forms.PictureBox appLogo;
+        private System.Windows.Forms.PictureBox appName;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnSettings;
+        private System.Windows.Forms.Button btnNotifications;
+        private System.Windows.Forms.Button btnMaterials;
+        private System.Windows.Forms.Button btnMembers;
+        private System.Windows.Forms.Button btnDashboard;
+        private System.Windows.Forms.Button btnLogout;
+        private System.Windows.Forms.Button btnLoggedUser;
     }
 }
 
