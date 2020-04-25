e947451d36599db4ce44a5d08b4677812f322905
Beis Presheva <beis.presheva@riinvest.net>
2020-04-25 22:30:53 +0200

    UI: This Login Form is making me SICK

--- MenaxhimiBibliotekes/Login Forms/LoginForm.Designer.cs
+++ MenaxhimiBibliotekes/Login Forms/LoginForm.Designer.cs
@@ -31,15 +31,15 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(loginForm));
             this.panelHeaderLogin = new System.Windows.Forms.Panel();
             this.picLogoLogin = new System.Windows.Forms.PictureBox();
+            this.btnCloseLogin = new System.Windows.Forms.Button();
             this.btnLogin = new System.Windows.Forms.Button();
             this.lblPasswordLogin = new System.Windows.Forms.Label();
             this.lblRightsReservedLogin = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.lblUsernameLogin = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
             this.txtPasswordLogin = new System.Windows.Forms.TextBox();
             this.txtUsernameLogin = new System.Windows.Forms.TextBox();
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.btnCloseLogin = new System.Windows.Forms.Button();
             this.panelHeaderLogin.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.picLogoLogin)).BeginInit();
             this.SuspendLayout();
@@ -50,30 +50,46 @@
             this.panelHeaderLogin.Controls.Add(this.picLogoLogin);
             this.panelHeaderLogin.Dock = System.Windows.Forms.DockStyle.Top;
             this.panelHeaderLogin.Location = new System.Drawing.Point(0, 0);
+            this.panelHeaderLogin.Margin = new System.Windows.Forms.Padding(2);
             this.panelHeaderLogin.Name = "panelHeaderLogin";
-            this.panelHeaderLogin.Size = new System.Drawing.Size(828, 474);
-            this.panelHeaderLogin.TabIndex = 2;
+            this.panelHeaderLogin.Size = new System.Drawing.Size(414, 246);
+            this.panelHeaderLogin.TabIndex = 3;
             // 
             // picLogoLogin
             // 
             this.picLogoLogin.BackColor = System.Drawing.Color.Transparent;
             this.picLogoLogin.Image = ((System.Drawing.Image)(resources.GetObject("picLogoLogin.Image")));
-            this.picLogoLogin.Location = new System.Drawing.Point(53, 188);
+            this.picLogoLogin.Location = new System.Drawing.Point(26, 98);
+            this.picLogoLogin.Margin = new System.Windows.Forms.Padding(2);
             this.picLogoLogin.Name = "picLogoLogin";
-            this.picLogoLogin.Size = new System.Drawing.Size(718, 104);
+            this.picLogoLogin.Size = new System.Drawing.Size(359, 54);
             this.picLogoLogin.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.picLogoLogin.TabIndex = 1;
             this.picLogoLogin.TabStop = false;
+            // 
+            // btnCloseLogin
+            // 
+            this.btnCloseLogin.BackColor = System.Drawing.Color.Maroon;
+            this.btnCloseLogin.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCloseLogin.ForeColor = System.Drawing.Color.White;
+            this.btnCloseLogin.Location = new System.Drawing.Point(129, 552);
+            this.btnCloseLogin.Margin = new System.Windows.Forms.Padding(2);
+            this.btnCloseLogin.Name = "btnCloseLogin";
+            this.btnCloseLogin.Size = new System.Drawing.Size(140, 42);
+            this.btnCloseLogin.TabIndex = 22;
+            this.btnCloseLogin.Text = "Close";
+            this.btnCloseLogin.UseVisualStyleBackColor = false;
             // 
             // btnLogin
             // 
             this.btnLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(55)))), ((int)(((byte)(132)))));
             this.btnLogin.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnLogin.ForeColor = System.Drawing.Color.White;
-            this.btnLogin.Location = new System.Drawing.Point(260, 948);
+            this.btnLogin.Location = new System.Drawing.Point(129, 493);
+            this.btnLogin.Margin = new System.Windows.Forms.Padding(2);
             this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(281, 81);
-            this.btnLogin.TabIndex = 14;
+            this.btnLogin.Size = new System.Drawing.Size(140, 42);
+            this.btnLogin.TabIndex = 23;
             this.btnLogin.Text = "Log In";
             this.btnLogin.UseVisualStyleBackColor = false;
             // 
@@ -82,10 +98,11 @@
             this.lblPasswordLogin.AutoSize = true;
             this.lblPasswordLogin.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblPasswordLogin.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.lblPasswordLogin.Location = new System.Drawing.Point(90, 734);
+            this.lblPasswordLogin.Location = new System.Drawing.Point(44, 382);
+            this.lblPasswordLogin.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblPasswordLogin.Name = "lblPasswordLogin";
-            this.lblPasswordLogin.Size = new System.Drawing.Size(153, 45);
-            this.lblPasswordLogin.TabIndex = 11;
+            this.lblPasswordLogin.Size = new System.Drawing.Size(76, 21);
+            this.lblPasswordLogin.TabIndex = 19;
             this.lblPasswordLogin.Text = "Password";
             // 
             // lblRightsReservedLogin
@@ -93,87 +110,81 @@
             this.lblRightsReservedLogin.AutoSize = true;
             this.lblRightsReservedLogin.Font = new System.Drawing.Font("Segoe UI", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblRightsReservedLogin.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.lblRightsReservedLogin.Location = new System.Drawing.Point(238, 1220);
+            this.lblRightsReservedLogin.Location = new System.Drawing.Point(118, 634);
+            this.lblRightsReservedLogin.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblRightsReservedLogin.Name = "lblRightsReservedLogin";
-            this.lblRightsReservedLogin.Size = new System.Drawing.Size(318, 30);
-            this.lblRightsReservedLogin.TabIndex = 12;
+            this.lblRightsReservedLogin.Size = new System.Drawing.Size(177, 13);
+            this.lblRightsReservedLogin.TabIndex = 20;
             this.lblRightsReservedLogin.Text = "All Rights Reserved ©Stackbooks";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.panel1.Location = new System.Drawing.Point(48, 331);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(309, 1);
+            this.panel1.TabIndex = 15;
             // 
             // lblUsernameLogin
             // 
             this.lblUsernameLogin.AutoSize = true;
             this.lblUsernameLogin.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblUsernameLogin.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.lblUsernameLogin.Location = new System.Drawing.Point(90, 520);
+            this.lblUsernameLogin.Location = new System.Drawing.Point(44, 270);
+            this.lblUsernameLogin.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblUsernameLogin.Name = "lblUsernameLogin";
-            this.lblUsernameLogin.Size = new System.Drawing.Size(163, 45);
-            this.lblUsernameLogin.TabIndex = 13;
+            this.lblUsernameLogin.Size = new System.Drawing.Size(81, 21);
+            this.lblUsernameLogin.TabIndex = 21;
             this.lblUsernameLogin.Text = "Username";
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.panel3.Location = new System.Drawing.Point(48, 443);
+            this.panel3.Margin = new System.Windows.Forms.Padding(2);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(309, 1);
+            this.panel3.TabIndex = 16;
             // 
             // txtPasswordLogin
             // 
             this.txtPasswordLogin.BackColor = System.Drawing.SystemColors.Control;
             this.txtPasswordLogin.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.txtPasswordLogin.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtPasswordLogin.Location = new System.Drawing.Point(98, 803);
+            this.txtPasswordLogin.Location = new System.Drawing.Point(48, 418);
+            this.txtPasswordLogin.Margin = new System.Windows.Forms.Padding(2);
             this.txtPasswordLogin.Name = "txtPasswordLogin";
             this.txtPasswordLogin.PasswordChar = '*';
-            this.txtPasswordLogin.Size = new System.Drawing.Size(618, 43);
-            this.txtPasswordLogin.TabIndex = 9;
+            this.txtPasswordLogin.Size = new System.Drawing.Size(309, 22);
+            this.txtPasswordLogin.TabIndex = 17;
             // 
             // txtUsernameLogin
             // 
             this.txtUsernameLogin.BackColor = System.Drawing.SystemColors.Control;
             this.txtUsernameLogin.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.txtUsernameLogin.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtUsernameLogin.Location = new System.Drawing.Point(98, 580);
+            this.txtUsernameLogin.Location = new System.Drawing.Point(48, 302);
+            this.txtUsernameLogin.Margin = new System.Windows.Forms.Padding(2);
             this.txtUsernameLogin.Name = "txtUsernameLogin";
-            this.txtUsernameLogin.Size = new System.Drawing.Size(618, 43);
-            this.txtUsernameLogin.TabIndex = 10;
-            // 
-            // panel3
-            // 
-            this.panel3.BackColor = System.Drawing.SystemColors.AppWorkspace;
-            this.panel3.Location = new System.Drawing.Point(98, 852);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(618, 1);
-            this.panel3.TabIndex = 8;
-            // 
-            // panel1
-            // 
-            this.panel1.BackColor = System.Drawing.SystemColors.AppWorkspace;
-            this.panel1.Location = new System.Drawing.Point(98, 636);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(618, 1);
-            this.panel1.TabIndex = 7;
-            // 
-            // btnCloseLogin
-            // 
-            this.btnCloseLogin.BackColor = System.Drawing.Color.Maroon;
-            this.btnCloseLogin.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCloseLogin.ForeColor = System.Drawing.Color.White;
-            this.btnCloseLogin.Location = new System.Drawing.Point(260, 1061);
-            this.btnCloseLogin.Name = "btnCloseLogin";
-            this.btnCloseLogin.Size = new System.Drawing.Size(281, 81);
-            this.btnCloseLogin.TabIndex = 14;
-            this.btnCloseLogin.Text = "Close";
-            this.btnCloseLogin.UseVisualStyleBackColor = false;
+            this.txtUsernameLogin.Size = new System.Drawing.Size(309, 22);
+            this.txtUsernameLogin.TabIndex = 18;
             // 
             // loginForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(828, 1284);
+            this.ClientSize = new System.Drawing.Size(414, 656);
             this.Controls.Add(this.btnCloseLogin);
-            this.Controls.Add(this.btnLogin);
             this.Controls.Add(this.panelHeaderLogin);
+            this.Controls.Add(this.btnLogin);
             this.Controls.Add(this.lblPasswordLogin);
-            this.Controls.Add(this.lblRightsReservedLogin);
-            this.Controls.Add(this.panel1);
-            this.Controls.Add(this.lblUsernameLogin);
-            this.Controls.Add(this.panel3);
-            this.Controls.Add(this.txtPasswordLogin);
             this.Controls.Add(this.txtUsernameLogin);
+            this.Controls.Add(this.lblRightsReservedLogin);
+            this.Controls.Add(this.txtPasswordLogin);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.lblUsernameLogin);
             this.Name = "loginForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "loginForm";
@@ -188,14 +199,14 @@
 
         private System.Windows.Forms.Panel panelHeaderLogin;
         private System.Windows.Forms.PictureBox picLogoLogin;
+        private System.Windows.Forms.Button btnCloseLogin;
         private System.Windows.Forms.Button btnLogin;
         private System.Windows.Forms.Label lblPasswordLogin;
         private System.Windows.Forms.Label lblRightsReservedLogin;
+        private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label lblUsernameLogin;
+        private System.Windows.Forms.Panel panel3;
         private System.Windows.Forms.TextBox txtPasswordLogin;
         private System.Windows.Forms.TextBox txtUsernameLogin;
-        private System.Windows.Forms.Panel panel3;
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Button btnCloseLogin;
     }
 }
