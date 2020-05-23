ead20c458b30e8abb0bfdc021f484fd5afe1cdb6
Beis Presheva <beis.presheva@riinvest.net>
2020-05-23 06:08:06 +0200

    notification commit

---
+++ MenaxhimiBibliotekes/Notifications Forms/NotificationsForm.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace MenaxhimiBibliotekes.Notifications_Forms
+{
+    partial class NotificationsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(NotificationsForm));
+            this.tableHeader = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.picTitle = new System.Windows.Forms.PictureBox();
+            this.panelSubHeader = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboNotificationType = new System.Windows.Forms.ComboBox();
+            this.panelBottom = new System.Windows.Forms.Panel();
+            this.flowpanelMain = new System.Windows.Forms.FlowLayoutPanel();
+            this.tableHeader.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picTitle)).BeginInit();
+            this.panelSubHeader.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableHeader
+            // 
+            this.tableHeader.BackColor = System.Drawing.Color.Teal;
+            this.tableHeader.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.tableHeader.ColumnCount = 3;
+            this.tableHeader.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableHeader.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 80F));
+            this.tableHeader.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableHeader.Controls.Add(this.panel1, 1, 0);
+            this.tableHeader.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tableHeader.Location = new System.Drawing.Point(0, 0);
+            this.tableHeader.Name = "tableHeader";
+            this.tableHeader.RowCount = 1;
+            this.tableHeader.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableHeader.Size = new System.Drawing.Size(740, 88);
+            this.tableHeader.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Controls.Add(this.lblTitle);
+            this.panel1.Controls.Add(this.picTitle);
+            this.panel1.Location = new System.Drawing.Point(143, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(453, 82);
+            this.panel1.TabIndex = 0;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.Color.White;
+            this.lblTitle.Location = new System.Drawing.Point(99, 17);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(301, 50);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "NOTIFICATIONS";
+            // 
+            // picTitle
+            // 
+            this.picTitle.Image = ((System.Drawing.Image)(resources.GetObject("picTitle.Image")));
+            this.picTitle.Location = new System.Drawing.Point(47, 17);
+            this.picTitle.Name = "picTitle";
+            this.picTitle.Size = new System.Drawing.Size(58, 50);
+            this.picTitle.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picTitle.TabIndex = 0;
+            this.picTitle.TabStop = false;
+            // 
+            // panelSubHeader
+            // 
+            this.panelSubHeader.Controls.Add(this.button1);
+            this.panelSubHeader.Controls.Add(this.comboNotificationType);
+            this.panelSubHeader.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelSubHeader.Location = new System.Drawing.Point(0, 88);
+            this.panelSubHeader.Name = "panelSubHeader";
+            this.panelSubHeader.Size = new System.Drawing.Size(740, 79);
+            this.panelSubHeader.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(48)))), ((int)(((byte)(115)))));
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(331, 15);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(137, 50);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Display";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // comboNotificationType
+            // 
+            this.comboNotificationType.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboNotificationType.FormattingEnabled = true;
+            this.comboNotificationType.Location = new System.Drawing.Point(73, 23);
+            this.comboNotificationType.Name = "comboNotificationType";
+            this.comboNotificationType.Size = new System.Drawing.Size(219, 33);
+            this.comboNotificationType.TabIndex = 0;
+            // 
+            // panelBottom
+            // 
+            this.panelBottom.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panelBottom.Location = new System.Drawing.Point(0, 589);
+            this.panelBottom.Name = "panelBottom";
+            this.panelBottom.Size = new System.Drawing.Size(740, 28);
+            this.panelBottom.TabIndex = 3;
+            // 
+            // flowpanelMain
+            // 
+            this.flowpanelMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowpanelMain.Location = new System.Drawing.Point(0, 167);
+            this.flowpanelMain.Name = "flowpanelMain";
+            this.flowpanelMain.Size = new System.Drawing.Size(740, 422);
+            this.flowpanelMain.TabIndex = 4;
+            // 
+            // NotificationsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(740, 617);
+            this.Controls.Add(this.flowpanelMain);
+            this.Controls.Add(this.panelBottom);
+            this.Controls.Add(this.panelSubHeader);
+            this.Controls.Add(this.tableHeader);
+            this.Name = "NotificationsForm";
+            this.Text = "NotificationsForm";
+            this.tableHeader.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picTitle)).EndInit();
+            this.panelSubHeader.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableHeader;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.PictureBox picTitle;
+        private System.Windows.Forms.Panel panelSubHeader;
+        private System.Windows.Forms.Panel panelBottom;
+        private System.Windows.Forms.FlowLayoutPanel flowpanelMain;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboNotificationType;
+    }
+}
---
+++ MenaxhimiBibliotekes/Notifications Forms/SpecifiedNotificationUC.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace MenaxhimiBibliotekes.Notifications_Forms
+{
+    partial class SpecifiedNotificationUC
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelSquare = new System.Windows.Forms.Panel();
+            this.pictureCategory = new System.Windows.Forms.PictureBox();
+            this.txtNotificationMessage = new System.Windows.Forms.Label();
+            this.txtNotificationDate = new System.Windows.Forms.Label();
+            this.txtNotificationCategory = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panelBorder = new System.Windows.Forms.Panel();
+            this.panelSquare.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureCategory)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelSquare
+            // 
+            this.panelSquare.Controls.Add(this.pictureCategory);
+            this.panelSquare.Location = new System.Drawing.Point(0, 0);
+            this.panelSquare.Name = "panelSquare";
+            this.panelSquare.Size = new System.Drawing.Size(87, 87);
+            this.panelSquare.TabIndex = 0;
+            // 
+            // pictureCategory
+            // 
+            this.pictureCategory.Location = new System.Drawing.Point(18, 19);
+            this.pictureCategory.Name = "pictureCategory";
+            this.pictureCategory.Size = new System.Drawing.Size(50, 50);
+            this.pictureCategory.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureCategory.TabIndex = 0;
+            this.pictureCategory.TabStop = false;
+            // 
+            // txtNotificationMessage
+            // 
+            this.txtNotificationMessage.AutoSize = true;
+            this.txtNotificationMessage.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNotificationMessage.Location = new System.Drawing.Point(89, 19);
+            this.txtNotificationMessage.Name = "txtNotificationMessage";
+            this.txtNotificationMessage.Size = new System.Drawing.Size(323, 21);
+            this.txtNotificationMessage.TabIndex = 1;
+            this.txtNotificationMessage.Text = "Notification Message is written in this part!";
+            // 
+            // txtNotificationDate
+            // 
+            this.txtNotificationDate.AutoSize = true;
+            this.txtNotificationDate.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNotificationDate.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.txtNotificationDate.Location = new System.Drawing.Point(90, 52);
+            this.txtNotificationDate.Name = "txtNotificationDate";
+            this.txtNotificationDate.Size = new System.Drawing.Size(76, 17);
+            this.txtNotificationDate.TabIndex = 1;
+            this.txtNotificationDate.Text = "01/01/2020";
+            // 
+            // txtNotificationCategory
+            // 
+            this.txtNotificationCategory.AutoSize = true;
+            this.txtNotificationCategory.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNotificationCategory.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.txtNotificationCategory.Location = new System.Drawing.Point(212, 52);
+            this.txtNotificationCategory.Name = "txtNotificationCategory";
+            this.txtNotificationCategory.Size = new System.Drawing.Size(142, 17);
+            this.txtNotificationCategory.TabIndex = 1;
+            this.txtNotificationCategory.Text = "Notification Category";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label4.Location = new System.Drawing.Point(177, 52);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(14, 17);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "||";
+            // 
+            // panelBorder
+            // 
+            this.panelBorder.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panelBorder.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelBorder.Location = new System.Drawing.Point(0, 0);
+            this.panelBorder.Name = "panelBorder";
+            this.panelBorder.Size = new System.Drawing.Size(862, 3);
+            this.panelBorder.TabIndex = 2;
+            // 
+            // SpecifiedNotificationUC
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.panelBorder);
+            this.Controls.Add(this.txtNotificationCategory);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtNotificationDate);
+            this.Controls.Add(this.txtNotificationMessage);
+            this.Controls.Add(this.panelSquare);
+            this.Name = "SpecifiedNotificationUC";
+            this.Size = new System.Drawing.Size(862, 87);
+            this.panelSquare.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureCategory)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelSquare;
+        private System.Windows.Forms.PictureBox pictureCategory;
+        private System.Windows.Forms.Label txtNotificationMessage;
+        private System.Windows.Forms.Label txtNotificationDate;
+        private System.Windows.Forms.Label txtNotificationCategory;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panelBorder;
+    }
+}
